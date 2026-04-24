f6defd6d1fdd4baa6265fd56751b5e92c4d58f7f
Wawrzyniec 'Wawrzek' Niewodniczański <main@wawrzek.name>
2026-04-24 20:32:34 +0100

    add css styles and temporary tests (#1)
    * introduces styles + temporary tests
    
    Move css to separate file and allow loading it.
    Make temporary tests for my local environment.
    Refactory portspage method to future proper tests
    
    * cosmetic changes
    
    - better expose key
    - better colours in black style

---
+++ tests/local_page.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import shutil
+import sys
+import os
+sys.path.append(os.path.abspath("../cruxpy"))
+sys.path.append(os.path.abspath(".."))
+from portspage import page
+
+if len(sys.argv) > 1:
+    style = sys.argv[1]
+else:
+    style = "original"
+
+page = page("../../wawrzek", style=style)
+page.write("/tmp/ports.html")
+
+shutil.copy(f"../cruxpy/files/{style}.css", "/tmp")
--- tests/simple.py
+++ tests/simple.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+
+import sys
+import os
+sys.path.append(os.path.abspath('../cruxpy'))
+sys.path.append(os.path.abspath('..'))
 from portspage import page
 
 page = page('../wawrzek')
